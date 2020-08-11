ad824b5ef2ce7aa55bc5c5af6ea74fe4ae092ed0
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-08-12 01:16:30 +0900

    Rename: addTextButton -> buttonToAddText, clearTextButton -> buttonToClearText
    Rename:
    addTextButtonとclearTextButtonは、動詞が先頭に来ているため関数名と勘違いする可能性があるので、buttonToAddTextというように明確な名前に変更した
    
    特に、前回addFuncToButton関数を作成した影響で、
    addFuncToButton(addTextButton, #selector(addText(_:)))
    というような、全てaddから始まる非常にややこしいコードになっていたので、早急に変える必要があった。

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -4,8 +4,8 @@ final class Question1ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var warningLabel: UILabel!
-    @IBOutlet weak var addTextButton: UIButton!
-    @IBOutlet weak var clearTextButton: UIButton!
+    @IBOutlet weak var buttonToAddText: UIButton!
+    @IBOutlet weak var buttonToClearText: UIButton!
     @IBOutlet weak var textView: UITextView!
     
     func addFuncToButton(_ button: UIButton!, _ selector: Selector) {
@@ -14,8 +14,8 @@ final class Question1ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addFuncToButton(addTextButton, #selector(addText(_:)))
-        addFuncToButton(clearTextButton, #selector(clearText(_:)))
+        addFuncToButton(buttonToAddText, #selector(addText(_:)))
+        addFuncToButton(buttonToClearText, #selector(clearText(_:)))
         textView.text = ""
         warningLabel.isHidden = true
     }
