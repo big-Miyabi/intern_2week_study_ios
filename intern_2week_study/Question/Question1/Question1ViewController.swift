10f1a02b3c32476e2d1b07ef1d7f6a1c3b62b706
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-08-12 00:18:51 +0900

    Add: 課題1-3をクリア(Warningの表示)

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 final class Question1ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var warningLabel: UILabel!
     @IBOutlet weak var addTextButton: UIButton!
     @IBOutlet weak var clearTextButton: UIButton!
     @IBOutlet weak var textView: UITextView!
@@ -12,9 +13,15 @@ final class Question1ViewController: UIViewController {
         addTextButton.addTarget(self, action: #selector(addText(_:)), for: UIControl.Event.touchUpInside)
         clearTextButton.addTarget(self, action: #selector(clearText(_:)), for: UIControl.Event.touchUpInside)
         textView.text = ""
+        warningLabel.isHidden = true
     }
     
     @objc func addText(_ sender: UIButton) {
+        if textField.text! == "" {
+            warningLabel.isHidden = false
+            return
+        }
+        warningLabel.isHidden = true
         textView.text += "\n" + textField.text!
     }
     
