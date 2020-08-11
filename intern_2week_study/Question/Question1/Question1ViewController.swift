020acd31528277624390742ad653534705826259
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-08-11 23:47:35 +0900

    Add: 課題1-2までクリア
    Add
    Addボタン押下時にtextFieldに入っている文字をtextViewに追加する処理を実装
    Clearボタン押下時にtextViewの文字を消す処理を実装

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -4,9 +4,21 @@ final class Question1ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var addTextButton: UIButton!
+    @IBOutlet weak var clearTextButton: UIButton!
     @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addTextButton.addTarget(self, action: #selector(addText(_:)), for: UIControl.Event.touchUpInside)
+        clearTextButton.addTarget(self, action: #selector(clearText(_:)), for: UIControl.Event.touchUpInside)
+        textView.text = ""
+    }
+    
+    @objc func addText(_ sender: UIButton) {
+        textView.text += "\n" + textField.text!
+    }
+    
+    @objc func clearText(_ sender: UIButton) {
+        textView.text = ""
     }
 }
