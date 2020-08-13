15bf84c8f360a9f087b49ff6ebaae3f5d26c72d2
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-08-13 15:41:06 +0900

    Improve: addFuncToButton関数を削除し、UIButtonのスコープ内にaddTargetの処理を加えた

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -4,8 +4,16 @@ final class Question1ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var warningLabel: UILabel!
-    @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var addButton: UIButton! {
+        didSet {
+            addButton.addTarget(self, action: #selector(addText(_:)), for: .touchUpInside)
+        }
+    }
+    @IBOutlet weak var clearButton: UIButton! {
+        didSet {
+            addButton.addTarget(self, action: #selector(addText(_:)), for: .touchUpInside)
+        }
+    }
     @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
