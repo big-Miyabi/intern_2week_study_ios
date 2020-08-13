fbc2915f2da1d096325d0b2a888048da8c9ef6ef
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-08-13 15:24:58 +0900

    Change: buttonToAddText -> addButton,  buttonToClearText -> clearButton Remove: storyboard上のUILabel内の初期値を空文字に変更し、それに伴いviewDidLoad()時のtextView.text = ""を削除した

--- intern_2week_study/Question/Question1/Question1ViewController.swift
+++ intern_2week_study/Question/Question1/Question1ViewController.swift
@@ -4,15 +4,14 @@ final class Question1ViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var warningLabel: UILabel!
-    @IBOutlet weak var buttonToAddText: UIButton!
-    @IBOutlet weak var buttonToClearText: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addFuncToButton(buttonToAddText, #selector(addText(_:)))
-        addFuncToButton(buttonToClearText, #selector(clearText(_:)))
-        textView.text = ""
+        addFuncToButton(addButton, #selector(addText(_:)))
+        addFuncToButton(clearButton, #selector(clearText(_:)))
         warningLabel.isHidden = true
     }
     
