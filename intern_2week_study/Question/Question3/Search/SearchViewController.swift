7c3bbbcadc1c20ca6a5ad5034451f9c1ecf2f9a6
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-08-17 01:54:43 +0900

    Add: ボタン押下中に色が変わるようにした
    Change
    storyboard上の色の設定をsRGBに統一し、swiftファイル内で色を変更した時とカラーコードに違いが出ないようにした

--- intern_2week_study/Question/Question3/Search/SearchViewController.swift
+++ intern_2week_study/Question/Question3/Search/SearchViewController.swift
@@ -3,7 +3,21 @@ import UIKit
 final class SearchViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var searchButton: UIButton! {
+        didSet {
+            searchButton.addTarget(self, action: #selector(darkenButton(_:)), for: .touchDown)
+            searchButton.addTarget(self, action: #selector(lightenButton(_:)), for: .touchUpInside)
+            searchButton.addTarget(self, action: #selector(lightenButton(_:)), for: .touchUpOutside)
+        }
+    }
+    
+    @objc func darkenButton(_ sender: UIButton) {
+        searchButton.backgroundColor = UIColor(red: 63 / 255, green: 149 / 255, blue: 0.0, alpha: 1.0)
+    }
+    
+    @objc func lightenButton(_ sender: UIButton) {
+        searchButton.backgroundColor = UIColor(red: 0, green: 201 / 255, blue: 0, alpha: 1.0)
+    }
     
     @IBAction func searchButtonTapped(_ sender: UIButton) {
         searchArticles()
@@ -13,7 +27,7 @@ final class SearchViewController: UIViewController {
         super.viewDidLoad()
         searchButton.layer.cornerRadius = 20.0
         searchButton.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        searchButton.layer.shadowColor = UIColor(red: 0.25, green: 0.29, blue: 0.23, alpha: 1).cgColor
+        searchButton.layer.shadowColor = UIColor(red: 65 / 255, green: 73 / 255, blue: 59 / 255, alpha: 1).cgColor
         searchButton.layer.shadowOpacity = 0.7
         searchButton.layer.shadowRadius = 2
     }
