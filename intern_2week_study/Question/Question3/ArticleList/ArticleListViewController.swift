ba452d90b5289d1a23f72bd406e0f76edc9dc0f2
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-08-16 18:25:50 +0900

    Add: 課題3-1までクリア(Qiitaの記事検索・一覧表示機能・ページ遷移の実装)

---
+++ intern_2week_study/Question/Question3/ArticleList/ArticleListViewController.swift
@@ -0,0 +1,51 @@
+import SafariServices
+import UIKit
+
+final class ArticleListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var articles: [Article] = []
+
+    static func makeInstance(_ articles: [Article]) -> ArticleListViewController {
+        let articleListViewController = R.storyboard.articleList.instantiateInitialViewController()!
+        articleListViewController.articles = articles
+        return articleListViewController
+    }
+
+    override func viewDidLoad() {
+        tableView.register(R.nib.articleListCell)
+    }
+}
+
+extension ArticleListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.articleListCell, for: indexPath),
+            let article = articles[safe: indexPath.row] else { return UITableViewCell() }
+        
+        cell.set(article)
+        return cell
+    }
+}
+
+extension ArticleListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        guard let urlString = articles[safe: indexPath.row]?.url,
+            let url = URL(string: urlString) else { return }
+        
+        let safariViewController = SFSafariViewController(url: url)
+        present(safariViewController, animated: true)
+    }
+}
--- intern_2week_study/Question/Question3/Search/SearchViewController.swift
+++ intern_2week_study/Question/Question3/Search/SearchViewController.swift
@@ -2,7 +2,42 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
+        searchArticles()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    private func searchArticles() {
+        // 多重タップ防止
+        searchButton.isEnabled = false
+        
+        guard let text = textField.text, !text.isEmpty else { return }
+        
+        APIClient.fetchArticles(keyword: text) { [weak self] result in
+            // URLSessionはbackground threadで行われる為UIの更新を明示的にMain Theadで行う
+            DispatchQueue.main.sync {
+                switch result {
+                case .success(let articles):
+                    self?.showArticleListScreen(articles)
+                    
+                case .failure(let error):
+                    let alert = UIAlertController.createErrorAlert(error)
+                    self?.present(alert, animated: true)
+                    
+                }
+                self?.searchButton.isEnabled = true
+            }
+        }
+    }
+    
+    private func showArticleListScreen(_ articles: [Article]) {
+        let articleListViewController = ArticleListViewController.makeInstance(articles)
+        navigationController?.pushViewController(articleListViewController, animated: true)
     }
 }
