a1f5d6746d5760e5e4de9269feea763bb6876abd
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-08-19 14:37:40 +0900

    Add: UIColor+Extensions.swiftを作成し、hexで色を指定できるようにした

--- intern_2week_study/Question/Question3/Search/SearchViewController.swift
+++ intern_2week_study/Question/Question3/Search/SearchViewController.swift
@@ -20,7 +20,7 @@ final class SearchViewController: UIViewController {
         searchArticles()
     }
     
-    final func beautifySearchButton() {
+    final private func beautifySearchButton() {
         searchButton.layer.cornerRadius = 15.0
         searchButton.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         searchButton.layer.shadowColor = UIColor(red: 65 / 255, green: 73 / 255, blue: 59 / 255, alpha: 1).cgColor
